d855f6aec34e9a9a0d96b0ecd65a5f98978678db
mheliot <maxime.heliotpro@gmail.com>
2022-03-21 01:48:09 +0100

    Initialize Physics + first tutorial steps

--- Movement/Physics/Assets/Scripts/MovingSphere.cs
+++ Movement/Physics/Assets/Scripts/MovingSphere.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 /// </summary>
 public class MovingSphere : MonoBehaviour
 {
+    #region Fields / Properties
+
     /// <summary>
     /// Instance variable <c>maxSpeed</c> represents the maximum speed value of the sphere.
     /// </summary>
@@ -18,21 +20,31 @@ public class MovingSphere : MonoBehaviour
     private float maxAcceleration = 10f;
 
     /// <summary>
-    /// Instance variable <c>allowedArea</c> is a Unity <c>Rect</c> structure representing the rectangle dimensions definition of the sphere allowed area.
-    /// </summary>
-    [SerializeField]
-    private Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
-
-    /// <summary>
-    /// Instance variable <c>bounciness</c> represents the bounciness value of the sphere.
-    /// </summary>
-    [SerializeField, Range(0f, 1f)]
-    private float bounciness = 0.5f;
-
-    /// <summary>
     /// Instance variable <c>velocity</c> is a Unity <c>Vector3</c> structure representing the velocity vector of the sphere.
     /// </summary>
     private Vector3 velocity;
+
+    /// <summary>
+    /// Instance variable <c>desiredVelocity</c> is a Unity <c>Vector3</c> structure representing the targeted velocity vector of the sphere.
+    /// </summary>
+    private Vector3 desiredVelocity;
+
+    /// <summary>
+    /// Instance variable <c>body</c> is a Unity <c>Rigidbody</c> component representing the rigidbody manager of the sphere.
+    /// </summary>
+    private Rigidbody body;
+
+    #endregion
+
+    #region MonoBehavior
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
 
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
@@ -49,38 +61,23 @@ public class MovingSphere : MonoBehaviour
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
 
         // Compute desired velocity depending player inputs.
-        Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+    }
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    private void FixedUpdate()
+    {
+        // Retrieve velocity from rigidbody before updating it. (Physics collisions and such affect velocity)
+        velocity = body.velocity;
         // Compute maximum speed change depending player max acceleration.
         float maxSpeedChange = maxAcceleration * Time.deltaTime;
         velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
         velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
 
-        Vector3 displacement = velocity * Time.deltaTime;
-        Vector3 newPosition = transform.localPosition + displacement;
-
-        // Clamp the sphere inside the area and apply a little bounciness effect on sphere colliding with the area limits.
-        if (newPosition.x < allowedArea.xMin)
-        {
-            newPosition.x = allowedArea.xMin;
-            velocity.x = -velocity.x * bounciness;
-        }
-        else if (newPosition.x > allowedArea.xMax)
-        {
-            newPosition.x = allowedArea.xMax;
-            velocity.x = -velocity.x * bounciness;
-        }
-
-        if (newPosition.z < allowedArea.yMin)
-        {
-            newPosition.z = allowedArea.yMin;
-            velocity.z = -velocity.z * bounciness;
-        }
-        else if (newPosition.z > allowedArea.yMax)
-        {
-            newPosition.z = allowedArea.yMax;
-            velocity.z = -velocity.z * bounciness;
-        }
-
-        transform.localPosition = newPosition;
+        body.velocity = velocity;
     }
+
+    #endregion
 }
